b75faba2882a8e82c8d9443cb3e1d30c62c3f968
Burakhan KATDAR <burakhan.katdar@bakircay.edu.tr>
2020-05-13 16:00:46 +0300

    Kişi Bilgileri Eklendi.

---
+++ VeriYapilari2/KisiBilgileri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje_Veri_Yapıları
+{
+    public class KisiBilgileri
+    {
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public string adres { get; set; }
+        public double telefon { get; set; }
+        public string email { get; set; }
+        public string uyruk { get; set; }
+        public int dogumTarihi { get; set; }
+        public string dogumYeri { get; set; } 
+        public string medeniDurum { get; set; }
+        public string yabanciDil { get; set; }
+        public string ilgliAlanlari { get; set; }
+        public int iseUygunlukDurumu { get; set; }
+
+    }
+}
